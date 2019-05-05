9463b705f27a690b456ed7e2002449261b153b9a
jobscale <jobscale@jsx.jp>
2019-05-05 09:21:50 +0900

    add shell spawn

--- shell/index.js
+++ shell/index.js
@@ -1,10 +1,16 @@
-const shell = require('child_process');
+const { spawn, exec } = require('child_process');
 const { App } = require('common');
 
 class Shell extends App {
   exec(cmd) {
     const promise = this.promise();
-    shell.exec(cmd, (e, stdout) => e ? promise.reject(e) : promise.resolve(stdout));
+    exec(cmd, (e, stdout) => e ? promise.reject(e) : promise.resolve(stdout));
+    return promise.instance;
+  }
+  spawn(command, params, options) {
+    const promise = this.promise();
+    const proc = spawn(command, params, Object.assign({ shell: true }, options));
+    proc.stdout.on('data', data => promise.resolve(data.toString()));
     return promise.instance;
   }
 }
